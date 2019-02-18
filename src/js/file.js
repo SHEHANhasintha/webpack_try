11160a7ce3ed99b4cb264d93a887810e7bfe257b
unknown <hasinthashehan768@gmail.com>
2019-02-18 23:06:57 +0530

    .

---
+++ src/js/file.js
@@ -0,0 +1,6 @@
+const second = require('./second');
+const $ = require('jquery');
+require('!style-loader!css-loader!../css/style.css');
+
+$('body').append('<h1>Component 1</h1>');
+alert('sesscond');
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,17 @@
 module.exports = {
   entry: {
-    app: './src/file.js',
+    app: './src/js/file.js',
   },
   output: {
     filename: 'webpack_output/webpack.js',
-    path: __dirname + '/'
+    path: __dirname + '/dist'
   },
-  mode: 'production'
+  mode: 'production',
+  module: {
+	    rules: [
+	      { test: /\.css$/, use: 'css-loader' },
+	      { test: /\.ts$/, use: 'ts-loader' },
+	      { test: /\.css$/, use: 'style-loader!css-loader'}
+	    ]
+	  }
 };
